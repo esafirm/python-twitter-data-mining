cd89a36191c900ab8ee04a5e937b7fc1976f4b92
Esa Firman <esafirm21@gmail.com>
2016-01-01 22:08:41 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'EhlHDoP2yXnkepTXF6IF1A'
+consumer_secret = '3DlKrOnpHQ7LHFw4nMRiKShedH1VPDihXJ4jFAMTwQ'
+access_token = '249775725-qkGfI8BEFOIJCOM5jOCsrh0O4irgtQoDAwFPFSok'
+access_secret = 'pmTWB9XwXbYTAl04n6Vc0qjA81R88Mvy5W4ASQKXs6HrL'
